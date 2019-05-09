0b672296454809e0eba6aacf702f54e9a9d2a887
MDIAS\ter01282 <yurekesley@gmail.com>
2019-05-09 15:58:28 -0300

    adicionando modelos

---
+++ src/main/java/br/yurekesley/jmssample/producer/ProdutoTopicProducer.java
@@ -0,0 +1,48 @@
+package br.yurekesley.jmssample.producer;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+import br.yurekesley.jmssample.model.Pedido;
+import br.yurekesley.jmssample.model.PedidoFactory;
+
+public class ProdutoTopicProducer {
+
+	public static void main(String[] args) throws Exception {
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		boolean transacted = false;
+		Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
+
+		Destination topico = (Destination) context.lookup("loja");
+
+		MessageProducer producer = session.createProducer(topico);
+
+		Pedido pedido = new PedidoFactory().geraPedidoComValores();
+
+		/**
+			StringWriter writer = new StringWriter();
+			JAXB.marshal(pedido, writer);
+			String xml = writer.toString();
+			Message message = session.createTextMessage(xml);
+		**/
+		
+		Message message = session.createObjectMessage(pedido);		
+		producer.send(message);
+
+		session.close();
+		connection.close();
+		context.close();
+
+	}
+
+}
---
+++ src/main/java/br/yurekesley/jmssample/receiver/TopicConsumerEstoqueSelector.java
@@ -0,0 +1,57 @@
+package br.yurekesley.jmssample.receiver;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class TopicConsumerEstoqueSelector {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws NamingException, JMSException {
+		InitialContext context = new InitialContext();
+
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = factory.createConnection();
+		connection.setClientID("estoque");
+		connection.start();
+
+		boolean transacted = false;
+		Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
+		Topic topic = (Topic) context.lookup("loja");
+		
+		
+		String selector = "ebook=false";
+		boolean noLocal = false;
+		MessageConsumer consumer = session.createDurableSubscriber(topic, "subscriber01", selector, noLocal);
+
+		consumer.setMessageListener(new MessageListener() {
+
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+
+				try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		new Scanner(System.in).nextLine(); // deixa programa em execução;
+		connection.close();
+		context.close();
+	}
+
+}
